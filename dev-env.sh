922f3c8efe06922deb98736d7f32be53d027a51b
hello-world-bfree <brandon@hallow.app>
2025-05-29 13:42:22 -0400

    Add intial set-up config.

---
+++ dev-env.sh
@@ -0,0 +1,60 @@
+#!/bin/bash bash
+dry_run="0"
+
+if [ -z "$XDG_CONFIG_HOME" ]; then
+    XDG_CONFIG_HOME=$HOME/.config
+fi
+
+if [ -z "$DEV_ENV" ]; then
+    echo "DEV_ENV not set but is required. Exiting..."
+    exit 1
+fi 
+
+if [[ $1 == "--dry" ]]; then
+    dry_run = "1"
+fi
+
+log() {
+    if [[ $dry_run == "1" ]]; then
+        echo "[DRY RUN]: $1"
+    else
+        echo "$1"
+    fi
+}
+
+log "env: $DEV_ENV"
+
+sync_directories() {
+    log "copying over files from: $1"
+    pushd $1 &> /dev/null
+    (
+        configs=`find . -mindepth 1 -maxdepth -type d`
+        for c in $configs; do
+            directory=${2%/}/${c#./}
+            log "   removing: rm -rf $directory"
+
+            if [[ $dry_run == "0" ]]; then
+                rm -rf $directory
+            fi
+
+            log "   copying env: cp $c $2"
+            if [[ $dry_run == "0" ]]; then
+                cp -r ./$c $2
+            fi
+        done
+    )
+    popd &> /dev/null
+}
+
+replace_file () {
+    log "removing: $2"
+    if [[ $dry_run == "0" ]]; then
+        rm $2
+    fi
+    log "copying: $1 to $2"
+    if [[ $dry_run == "0" ]]; then
+        cp $1 $2
+    fi
+}
+
+replace_file $DEV_ENV/env/.bashrc $HOME/.bashrc
---
+++ script_runner.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+if [ -z "$XDG_CONFIG_HOME" ]; then
+    XDG_CONFIG_HOME=$HOME/.config
+fi
+
+if [ -z "$DEV_ENV" ]; then
+    echo "env var DEV_ENV needs to be present"
+    exit 1
+fi
+
+# if i just did DEV_ENV=$(pwd) ./run then this is needed for the rest of the
+# scripts
+export DEV_ENV="$DEV_ENV"
+
+grep=""
+dry_run="0"
+
+while [[ $# -gt 0 ]]; do
+    echo "ARG: \"$1\""
+    if [[ "$1" == "--dry" ]]; then
+        dry_run="1"
+    else
+        grep="$1"
+    fi
+    shift
+done
+
+log() {
+    if [[ $dry_run == "1" ]]; then
+        echo "[DRY_RUN]: $1"
+    else
+        echo "$1"
+    fi
+}
+
+log "RUN: env: $env -- grep: $grep"
+
+# Remember that all scripts that we want to run must have their executable permissions set
+# i.e., chmod +x ./scripts/{script_name}.sh
+scripts_dir=`find $script_dir/scripts -mindepth 1 -maxdepth 1 -type f -perm +111` 
+
+log "$scripts_dir"
+
+for s in $scripts_dir; do
+    if basename $s | grep -vq "$grep"; then
+        log "grep \"$grep\" filtered out $s"
+        continue
+    fi
+
+    log "running script: $s"
+
+    if [[ $dry_run == "0" ]]; then
+        sh $s
+    fi
+done
